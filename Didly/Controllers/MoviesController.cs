9fbc6068b3a50aebb87bef5766e39de1f7afa9ab
dheeraj <djshres@gmail.com>
2019-02-07 11:41:38 +0545

    Added Movies Controller

---
+++ Didly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using Didly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Didly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movie = new Movie()
+            {
+                Name = "Titanic!"
+            };
+            return View(movie);
+        }
+    }
+}
